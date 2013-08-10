28dc50909cf1a7e3e331e9f931c0fdbe173bee04
oscr <oscar.utbult@gmail.com>
2013-08-10 21:58:36 +0200

    Backup commit. Improving documentation for Controller.

--- AndroidChess/src/main/java/io/oscr/androidchess/controller/ChessController.java
+++ AndroidChess/src/main/java/io/oscr/androidchess/controller/ChessController.java
@@ -8,17 +8,50 @@ import io.oscr.androidchess.model.event.PromotionEvent;
 import io.oscr.androidchess.model.pieces.PieceColor;
 import io.oscr.androidchess.model.pieces.PieceType;
 
-public class ChessController {
-	private IChessModel model;
-	
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Immutable in the sense that the model reference never changes. Marked as final.
+ *
+ * Controller class in the MVC pattern. Simply delegates invocations from the view to the model.
+ * Contains no other state than reference to model.
+ */
+public final class ChessController {
+	private final IChessModel model;
+
+    /**
+     * Creates an Controller object.
+     *
+     * Checks argument and will throw NullPointerException if argument should be null. No
+     * other sanity checks are performed.
+     *
+     * @throws NullPointerException if model argument is null.
+     * @param model to delegate invocations to.
+     */
 	public ChessController(IChessModel model){
+        checkNotNull(model, "Argument model was null. Expected non null");
 		this.model = model;
 	}
-	
+
+    /**
+     * Used for determining the background color of a chess board square. Given the zero indexed
+     * file and rank will return a ColorDrawable for that position of the chessboard.
+     *
+     * @param file zero indexed
+     * @param rank zero indexed
+     * @return a ColorDrawable for the specified
+     */
 	public ColorDrawable getBoardColor(int file, int rank) {
 		return model.getBoardColor(new BoardPosition(file, rank));
 	}
 
+
+    /**
+     *
+     * @param file
+     * @param rank
+     * @return
+     */
 	public String getPieceString(int file, int rank) {
 		return model.getPieceString(new BoardPosition(file, rank));
 	}
